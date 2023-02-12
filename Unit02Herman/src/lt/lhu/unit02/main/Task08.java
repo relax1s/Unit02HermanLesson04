1ad1cd286b7bba6a90badd674bae7ebdf1bfb338
relax1s <123899997+relax1s@users.noreply.github.com>
2023-02-12 12:53:06 +0300

    Add files via upload

---
+++ Unit02Herman/src/lt/lhu/unit02/main/Task08.java
@@ -0,0 +1,19 @@
+package lt.lhu.unit02.main;
+
+public class Task08 {
+
+	public static void main(String[] args) {
+		System.out.println("Решаем задачу 8");
+
+		int x;
+		double fx;
+
+		x = 2;
+
+		fx = 2 * Math.tan(x / 2) + 1;
+
+		System.out.println("F(x)=" + fx);
+
+	}
+
+}
---
+++ Unit02Herman/src/lt/lhu/unit02/main/Task09.java
@@ -0,0 +1,22 @@
+package lt.lhu.unit02.main;
+
+public class Task09 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Решаем задачу 9");
+
+		int x;
+		double fx;
+
+		x = 4;
+		if (x <= -3) {
+			fx = 9;
+
+		} else {
+			fx = 1 / x * x + 1;
+
+			System.out.println("F(x)=" + fx);
+		}
+	}
+}
